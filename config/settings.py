e12180066fd68e0103a27b8915d5e483f020d340
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-12-23 08:20:32 -0300

    add config class

---
+++ config/settings.py
@@ -0,0 +1,26 @@
+import os
+from datetime import timedelta
+from decouple import config
+
+DEBUG = config("FLASK_DEBUG", cast=bool, default=False)
+
+EXTENSIONS = [
+    "src.ext.database",
+    "src.ext.serializer",
+    "src.ext.migration",
+    "src.ext.token",
+    "src.ext.cors",
+    "src.blueprints.api_v1",
+]
+
+THIS_PATH = os.path.dirname(__file__)
+PROJECT_ROOT = os.path.abspath(os.path.dirname(THIS_PATH))
+SECRET_KEY = config("SECRET_KEY")
+
+SQLALCHEMY_DATABASE_URI = config("SQLALCHEMY_DATABASE_URI")
+SQLALCHEMY_BINDS = eval(config("DATABASE_BINDS"))
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+JWT_SECRET_KEY = config("JWT_SECRET_KEY")
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(30)
+JWT_REFRESH_TOKEN_EXPIRES = timedelta(60)
