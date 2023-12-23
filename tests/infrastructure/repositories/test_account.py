9a06707e1cbfc2838f1a7da7b7685a2f84713aef
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-12-23 08:30:40 -0300

    unit tests files

---
+++ tests/infrastructure/repositories/test_account.py
@@ -0,0 +1,131 @@
+from src.domain.entities.account import Account
+from src.infrastructure.database.account import AccountTable
+
+
+def test_create_account(account_repository, person):
+    account_data = {
+        "person_id": person.id,
+        "account_type": "savings",
+        "daily_withdrawal_limit": 500.0,
+    }
+    account = Account(**account_data)
+
+    created_account = account_repository.create(account)
+
+    assert created_account.id is not None
+    assert created_account.person_id == account_data["person_id"]
+    assert created_account.account_type == account_data["account_type"]
+    assert (
+        created_account.daily_withdrawal_limit == account_data["daily_withdrawal_limit"]
+    )
+
+
+def test_create_account_commit(db, account_repository, person):
+    account_data = {
+        "person_id": person.id,
+        "account_type": "savings",
+        "daily_withdrawal_limit": 500.0,
+    }
+    account = Account(**account_data)
+
+    created_account = account_repository.create(account)
+
+    db.session.commit()
+    with db.session.no_autoflush:
+        retrieved_account = db.session.get(AccountTable, created_account.id)
+        assert retrieved_account is not None
+
+
+def test_get_by_person_id_returns_account_when_found(
+    account_repository, create_account, person
+):
+    created_account = create_account(person_id=person.id, account_type=1)
+
+    retrieved_account = account_repository.get_by_person_id(
+        person_id=person.id,
+    )
+
+    assert retrieved_account.id == created_account.id
+
+
+def test_get_by_person_id_returns_none_when_not_found(account_repository):
+    person_id = 456
+
+    retrieved_account = account_repository.get_by_person_id(person_id)
+
+    assert retrieved_account is None
+
+
+def test_get_by_args_returns_account_when_found(
+    account_repository, create_account, person
+):
+    account = create_account(account_type=1, person_id=person.id)
+
+    retrieved_account = account_repository.get_by_args(account_type=1)
+
+    assert retrieved_account.id == account.id
+
+
+def test_get_by_args_returns_none_when_not_found(account_repository):
+    retrieved_account = account_repository.get_by_args(account_type=2)
+
+    assert retrieved_account is None
+
+
+def test_get_accounts_info_returns_correct_total_and_count(
+    account_repository, create_account, person
+):
+    create_account(amount=100, account_type=2, person_id=person.id)
+
+    create_account(amount=200, account_type=1, person_id=person.id)
+
+    total_amount, account_count = account_repository.get_accounts_info(
+        person_id=person.id
+    )
+
+    assert total_amount == 0
+    assert account_count == 2
+
+
+def test_get_accounts_info_returns_zero_when_no_accounts_found(account_repository):
+    total_amount, account_count = account_repository.get_accounts_info(456)
+
+    assert total_amount == 0
+    assert account_count == 0
+
+
+def test_filter_returns_accounts_matching_criteria(
+    account_repository, create_account, person
+):
+    create_account(account_type=1, person_id=person.id)
+    create_account(account_type=1, person_id=person.id)
+    create_account(account_type=2, person_id=person.id)
+
+    filtered_accounts = account_repository.filter(account_type=1)
+
+    assert len(filtered_accounts) == 2
+
+
+def test_filter_returns_empty_list_when_no_accounts_match(account_repository):
+    filtered_accounts = account_repository.filter(account_type=1)
+
+    assert filtered_accounts == []
+
+
+def test_update_account_updates_fields_correctly(
+    account_repository, create_account, person
+):
+    account = create_account(person_id=person.id, account_type=2)
+
+    updated_account = account_repository.update_account(
+        account.id, daily_withdrawal_limit=500, account_type=3
+    )
+
+    assert updated_account.daily_withdrawal_limit == 500
+    assert updated_account.account_type == 3
+
+
+def test_update_account_returns_none_when_account_not_found(account_repository):
+    updated_account = account_repository.update_account(123, amount=500)
+
+    assert updated_account is None
---
+++ tests/infrastructure/repositories/test_transaction.py
@@ -0,0 +1,73 @@
+from datetime import date
+from src.infrastructure.database.transaction import TransactionTable
+from src.domain.entities import Transaction
+
+
+def test_create_transaction(transaction_repository):
+    transaction_data = {
+        "account_id": 1,
+        "value": 100.0,
+        "type": "credit",
+    }
+    transaction = Transaction(**transaction_data)
+
+    created_transaction = transaction_repository.create(transaction)
+
+    assert created_transaction.id is not None
+    assert created_transaction.account_id == transaction_data["account_id"]
+    assert created_transaction.value == transaction_data["value"]
+    assert created_transaction.type == transaction_data["type"]
+
+
+def test_get_aggregate_value_in_one_day(transaction_repository):
+    account_id = 1
+    today = date.today()
+    transactions_data = [
+        {
+            "value": 50.0,
+            "type": "-",
+            "account_id": account_id,
+            "transaction_date": today,
+        },
+        {
+            "value": 30.0,
+            "type": "-",
+            "account_id": account_id,
+            "transaction_date": today,
+        },
+        {
+            "value": 20.0,
+            "type": "+",
+            "account_id": account_id,
+            "transaction_date": today,
+        },
+    ]
+
+    for data in transactions_data:
+        transaction = TransactionTable(**data)
+        transaction_repository.session.add(transaction)
+
+    transaction_repository.session.commit()
+
+    total_value = transaction_repository.get_aggregate_value_in_one_day(account_id)
+
+    assert total_value == 80.0
+
+
+def test_get_aggregate_value_in_one_day_no_transactions(transaction_repository):
+    account_id = 2
+    total_value = transaction_repository.get_aggregate_value_in_one_day(account_id)
+
+    assert total_value == 0.0
+
+
+def test_filter_transactions_by_person_no_transactions(transaction_repository, person):
+    (
+        transactions,
+        cash_in,
+        cash_out,
+    ) = transaction_repository.filter_transactions_by_person(person_id=person.id)
+
+    assert transactions is None
+    assert cash_in == 0
+    assert cash_out == 0
