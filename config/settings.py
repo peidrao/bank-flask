3eee6cd62def00b715b994a9bc231822c8502bb6
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-12-24 10:03:23 -0300

    refactor configurations

--- config/settings.py
+++ config/settings.py
@@ -2,24 +2,28 @@ import os
 from datetime import timedelta
 from decouple import config
 
-DEBUG = config("FLASK_DEBUG", cast=bool, default=False)
 
-EXTENSIONS = [
-    "src.ext.database",
-    "src.ext.serializer",
-    "src.ext.migration",
-    "src.ext.token",
-    "src.ext.cors",
-    "src.blueprints.api_v1",
-]
+class Config(object):
+    DEBUG = config("FLASK_DEBUG", cast=bool, default=False)
 
-THIS_PATH = os.path.dirname(__file__)
-PROJECT_ROOT = os.path.abspath(os.path.dirname(THIS_PATH))
-SECRET_KEY = config("SECRET_KEY")
+    EXTENSIONS = [
+        "src.ext.database",
+        "src.ext.serializer",
+        "src.ext.migration",
+        "src.ext.token",
+        "src.ext.cors",
+        "src.blueprints.api_v1",
+    ]
 
-SQLALCHEMY_DATABASE_URI = config("SQLALCHEMY_DATABASE_URI")
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+    THIS_PATH = os.path.dirname(__file__)
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(THIS_PATH))
+    SECRET_KEY = config("SECRET_KEY")
 
-JWT_SECRET_KEY = config("JWT_SECRET_KEY")
-JWT_ACCESS_TOKEN_EXPIRES = timedelta(30)
-JWT_REFRESH_TOKEN_EXPIRES = timedelta(60)
+    SQLALCHEMY_DATABASE_URI = config("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    JWT_SECRET_KEY = config("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(30)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(60)
+
+    BANK_REQUESTS = config("BANK_REQUESTS")
