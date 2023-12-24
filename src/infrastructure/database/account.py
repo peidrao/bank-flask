0dd3814dfd78a53bff18beea9cdb4db75c907a3e
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-12-24 10:07:21 -0300

    refactor account model

--- src/infrastructure/database/account.py
+++ src/infrastructure/database/account.py
@@ -1,15 +1,11 @@
-from datetime import datetime
 from src.ext.database import db
+from .base import BaseTable
 
 
-class AccountTable(db.Model):
+class AccountTable(BaseTable):
     __tablename__ = "accounts"
 
-    id = db.Column(db.Integer, primary_key=True)
-    person_id = db.Column(db.Integer, db.ForeignKey("persons.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     amount = db.Column(db.Float, default=0.0)
     daily_withdrawal_limit = db.Column(db.Float, default=0.0)
-    is_active = db.Column(db.Boolean, default=True)
-    account_type = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
     transactions = db.relationship("TransactionTable", backref="accounts", lazy=True)
