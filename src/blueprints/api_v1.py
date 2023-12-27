7aed75db2966546de5aa83645bc0f71e1289be0b
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-12-27 12:08:58 -0300

    refactor blueprint

--- src/blueprints/api_v1.py
+++ src/blueprints/api_v1.py
@@ -1,14 +1,21 @@
-from src.apps.api.routes import (
-    auth_blueprint,
-    user_blueprint,
-    account_blueprint,
-    transaction_blueprint,
-)
+from src.apps.api.routes import BLUEPRINTS, API_PREFIX
+from flask import Blueprint
+from flask_restful import Api
 
 
 def init_app(app):
     """Initialize blueprints"""
-    app.register_blueprint(auth_blueprint)
-    app.register_blueprint(user_blueprint)
-    app.register_blueprint(account_blueprint)
-    app.register_blueprint(transaction_blueprint)
+
+    def register_resources(api, resources_list):
+        for resource, route in resources_list:
+            api.add_resource(resource, route)
+
+    for blueprint_name, blueprint_url, resources_list in BLUEPRINTS:
+        blueprint = Blueprint(
+            blueprint_name,
+            __name__,
+            url_prefix=f"{API_PREFIX}{blueprint_url}"
+        )
+        api = Api(blueprint)
+        register_resources(api, resources_list)
+        app.register_blueprint(blueprint)
